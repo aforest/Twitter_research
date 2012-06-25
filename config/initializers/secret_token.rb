7fc4180833fb65e74513dca1a573c47b9e954175
Austin Forest <cforest@pattonboggs.com>
2012-06-25 08:49:45 -0600

    this is a rails frame work for my twitter search application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterResearch::Application.config.secret_token = '9ca5d8448c8e2ed833a5022370a2b4e2be12f83e7a48f3d2faccfcf7397df494550648613d4aaf8b735cd17dc32b2297bfb477f5f625785f3aacda2da195834a'
